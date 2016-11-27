20c622402f7cb77ce8191dab02ea70a1b19e0dac
Sam Whillance <samwhillance@gmail.com>
2016-11-27 21:26:56 +1100

    alerts

--- public/js/app.js
+++ public/js/app.js
@@ -14,13 +14,16 @@ app.service("Entries", function ($http) {
 app.controller("appController", ['$scope', '$log', 'Entries', function ($scope, $log, Entries) {
 	$scope.barcode = null;
 	$scope.entries = [];
+	$scope.alert = null;
+
+	var alertTimer = null;
 
 	$scope.getEntries = function () {
 		Entries.getEntries().then(function (response) {
 			$log.info("Entries", response.data);
 			$scope.entries = response.data;
 		}, function (error) {
-			alert("Error getting entries.");
+			$scope.setAlert("alert-danger", "Failed to get items from database");
 		});
 	};
 
@@ -31,12 +34,34 @@ app.controller("appController", ['$scope', '$log', 'Entries', function ($scope, 
 
 		Entries.createEntry(entry).then(function (response) {
 			$log.info("Entry created");
+			$scope.setAlert("alert-success", "Item was added");
 			$scope.getEntries();
 		}, function (error) {
 			alert("Error creating entry.");
+			$scope.setAlert("alert-danger", "Failed to add item to database");
 		});
 
 		$scope.barcode = "";
 	};
+
+	$scope.setAlert = function(aClass, aMessage){
+		$scope.alert = {
+			class: aClass,
+			message: aMessage
+		};
+
+		alertEndLife();
+	};
+
+	function alertEndLife(){
+		if (alertTimer){
+			clearTimeout(alertTimer);
+			alertTimer = null;
+		}
+
+		alertTimer = setTimeout(function(){
+			$scope.alert = null;
+		}, 5000);
+	}
 
 }]);
